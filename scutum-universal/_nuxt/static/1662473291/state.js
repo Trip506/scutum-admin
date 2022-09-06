e8c06c5f42705b2ceaf5a89a24f1f5fbab59863f
Samuel <>
2022-09-06 15:10:44 +0100

    another commit

---
+++ scutum-universal/_nuxt/static/1662473291/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fscutum-universal\u002F_nuxt\u002Fstatic\u002F1662473291",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fscutum-universal\u002F",assetsPath:"\u002Fscutum-universal\u002F_nuxt\u002F",cdnURL:a}}}}(null));
